8ca62dcb32c2c59a318d092c5bc8ed5ec398de3a
MadhavBarthapuram <madhav.barthapuram@gmail.com>
2024-12-04 22:28:34 +0530

    minor changes

--- OTP_Generator.java
+++ OTP_Generator.java
@@ -1,7 +1,8 @@
 package day_2;
 public class OTP_Generator {
 	public static void main(String[] args) {
+		int x=math.random();
 		double ran=(Math.random());
-		System.out.println(ran);
+		System.out.println(ran+x);
 	}
 }
